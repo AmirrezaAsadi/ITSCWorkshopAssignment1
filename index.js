bca7efa4bfb770cd7cd2ace0ae766d6e0b59f1cf
unknown <your_no_reply_email@users.noreply.github.com>
2023-12-12 09:49:01 -0500

    switch based operation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const subButton = document.getElementById('substractButton');
 const multiplyButton = document.getElementById('multiplyButton');
 const divideButton = document.getElementById('divideButton');
 
+const buttons = document.querySelectorAll('button');
+
 const var1=document.querySelector('#var1')
 const var2=document.querySelector('#var2')
 const outputList = document.getElementById('resultList')
@@ -22,6 +24,34 @@ function add(num1, num2) {
 function subtract(num1, num2) {
     return num1 - num2;
 }
+
+function operate(operation, num1, num2) {
+    switch(operation) {
+        case 'addButton':
+            return num1 + num2;
+        case 'substractButton':
+            return num1 - num2;
+        case 'multiplyButton':
+            return num1 * num2;
+        case 'divideButton':
+            return num1 / num2;
+        default:
+            return 0;
+    }
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonId = button.id;
+        const result = operate(buttonId, parseFloat(var1.value), parseFloat(var2.value));
+        let newItem = document.createElement("li");
+        newItem.innerText = result;
+        outputList.appendChild(newItem);
+        outputList.append("\n");
+    });
+});
+
+/*
 
 addButton.addEventListener('click',()=>{
     let newItem = document.createElement("li");
@@ -62,9 +92,4 @@ subButton.addEventListener('click',()=>{
 console.log(multiplyButton)
 
 
-/* 
- var newItem = document.createElement("li");
-    newItem = add(var1,var2);
-    outputList.appendChild(newItem);
-    console.log("added");
-    */
+*/
